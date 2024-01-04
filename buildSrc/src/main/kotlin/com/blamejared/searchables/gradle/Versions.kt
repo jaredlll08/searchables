be8df3ed0d97a373cb67436bb23eb5aa4bdfe0a3
rockbyo5 <rockbyo5@gmail.com>
2024-01-03 22:46:10 -0500

    Port to 1.20.4

--- buildSrc/src/main/kotlin/com/blamejared/searchables/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/searchables/gradle/Versions.kt
@@ -3,11 +3,11 @@ package com.blamejared.searchables.gradle
 object Versions {
     const val MOD = "1.0"
     const val JAVA = "17"
-    const val MINECRAFT = "1.20.3"
+    const val MINECRAFT = "1.20.4"
     const val FABRIC_LOADER = "0.15.3"
-    const val FABRIC = "0.91.1+1.20.3"
-    const val FORGE = "49.0.2"
+    const val FABRIC = "0.91.1+1.20.4"
+    const val FORGE = "49.0.14"
     const val FORGE_LOADER = "[49,)"
-    const val NEO_FORGE = "20.3.8-beta"
+    const val NEO_FORGE = "20.4.75-beta"
     const val NEO_FORGE_LOADER= "[1,)"
 }
